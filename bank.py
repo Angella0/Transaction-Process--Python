2ddaa7e4011bb59282c10e1ab7d6a5d9c615185f
Angella0 <angellasimbwa@gmail.com>
2021-05-18 07:24:53 +0300

    commit

---
+++ bank.py
@@ -0,0 +1,9 @@
+class Bank:
+    def __init__(self,name,village):
+        self.name = name
+        self.village = village
+    def deposit_money(self):
+        return "You have deposited 100,000" 
+    def withdraw(self):
+        return "You have withdrawn 8000, your balance is 92000"       
+    
---
+++ car.py
@@ -0,0 +1,14 @@
+class Car:
+
+    def __init__(self,model,make):
+        self.model = model
+        self.make = make
+        
+    def start(self):
+        return f"vuuuum, vuuum"
+    def accelerate(self,accelerate,speed):
+        return accelerate+speed   
+
+
+
+        
