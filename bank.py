52a7bfaa7645f1b2a12dc78d74fc410c04bceb59
Angella0 <angellasimbwa@gmail.com>
2021-06-17 09:10:22 +0300

    commit

--- bank.py
+++ bank.py
@@ -7,8 +7,12 @@ class Account:
         self.statement = []
         self.loan = 0
     def Show_balance(self):
-        return f"Hello {self.name} your balance is {self.balance}" 
+        return f"Hello {self.name} you have deposited {self.balance}" 
     def deposit(self,amount):
+        try:
+            10 + amount
+        except TypeError:
+            return f"The amount must be in figures"
         if amount <= 0:
             return f"you cannot deposit less than 0"
         else:
@@ -23,6 +27,10 @@ class Account:
              self.statement.append(transaction)
              return self.Show_balance () 
     def withdraw(self,money):
+        try:
+            4-money
+        except TypeError:
+            return f"you can not withdaw string"
         if money > self.balance:
             return f" your balance is {self.balance} you cannot withdraw {money}"
         else:
@@ -38,12 +46,8 @@ class Account:
             return self.Show_balance () 
              
 
-    # def borrow(self,amount):
-    #     return f" Hey {self.name} you have successfully borrowed {amount}"
-
-    # def pay_loan(self,amount):
-    #     return f"Hello, {self.name} you have deposited {amount}"
-
+    
+    
     def show_statement(self):
 
         for transaction in self.statement:
@@ -55,6 +59,11 @@ class Account:
             return
 
     def borrow(self,amount):
+        try:
+            7 + amount
+        except TypeError:
+            return f"You must input a figure not a word"
+    
         if amount < 0:
             return f"You do not qaulify for this amount of{amount}"
         elif self.loan > 0:
@@ -76,11 +85,16 @@ class Account:
             return f"You have successfully borrowed {self.loan}"
 
     def repay (self,amount):
+        try:
+            5*amount
+        except TypeError:
+            return f"The amount must be in figures"
         if amount < 0:
             return "You can not pay this loan"
         elif amount <= self.loan:
-             self.loan -= amount
-             return f"You have paid {amount} and your loan is {self.loan}"
+            loan=self.loan-amount
+            self.loan = loan
+            return f"You have paid {amount} and your loan is {self.loan}"
         elif amount>self.loan:
             diff = amount - self.loan
             self.loan = 0
@@ -93,7 +107,56 @@ class Account:
 
              }
             self.statement.append(transaction)
-            return f"You have successfully paid your loan and your current balance is{self.deposit}"
+            return f"You have successfully paid your loan and your current balance is{self.balance}"
+
+    def transfer(self,account,amount):
+        self.account=account
+        self.amount=amount
+        fee=amount*0.005
+        total=fee+amount
+        try:
+            6 + amount
+        except TypeError:
+            return f"The amount should be in figures" 
+      
+        if total > self.balance:
+            return f" your balance is {self.balance} and you need at least {total} for this transaction"
+        else:
+            self.balance-=total
+            account.deposit(amount)
+            now = datetime.now() 
+            transaction ={
+                 "amount": amount,
+                 "time": now,
+                 "Narration":"you have transferred:"
+
+             }
+            self.statement.append(transaction)
+            return f"You have transferred {amount}. "
+class MobileMoneyAccount(Account):
+    def __init__(self,name,phoneNumber,services_provider):
+        Account. __init__(self,name,phoneNumber)
+        self.services_provider = services_provider
+
+    def buy_airtime(self,amount):
+        try:
+            10+amount
+        except TypeError:
+            return f"the amount must be in figures"
+        if amount < 0:
+            return f"your amount is not enough to buy the airtime"
+        elif self.balance<amount:
+            return f"your balance is low to purchase this amount of credit"
+        else:
+            self.balance-=amount
+            return f"you have purchased airtime worth {amount} from {self.services_provider}. Your account balance is {self.balance}"
+                     
+             
+
+
+
+    
+
         
 
 
